569731204c60f48924555a67092a499f0a262c9e
Bob Bell <bwbelljr@gmail.com>
2021-02-17 01:58:03 +0100

    fix: add dataset class for birds

--- utils/mypath.py
+++ utils/mypath.py
@@ -13,15 +13,18 @@ class MyPath(object):
 
         if database == 'cifar-10':
             return '/path/to/cifar-10/'
-        
+
         elif database == 'cifar-20':
             return '/path/to/cifar-20/'
 
         elif database == 'stl-10':
             return '/path/to/stl-10/'
-        
+
         elif database in ['imagenet', 'imagenet_50', 'imagenet_100', 'imagenet_200']:
             return '/path/to/imagenet/'
+
+        elif database == 'birds':
+            return '/content/Unsupervised-Classification/data/CUB_200_2011/images/'
         
         else:
             raise NotImplementedError
